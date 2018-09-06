cfb2def7a749561568b22c24dc4b7cc5d6f3b3ad
LeTranAnhVu <vule5726@gmail.com>
2018-09-06 23:42:02 +0700

    gui add log area.

--- gui.py
+++ gui.py
@@ -1,7 +1,7 @@
 from tkinter import *
 
 window = Tk()
-window.geometry("400x300")
+window.geometry("500x250")
 window.title("Replace Tool")
 
 
@@ -14,18 +14,26 @@ def btnReplace_Click():
 
 
 # create form
+lblPath = Label(window, text="Add Path")
 lblSearch = Label(window, text="Search for")
 lblReplace = Label(window, text="Replaced by")
+lblLog = Label(window, text="Log:")
+txtPath = Entry(window, width=30)
 txtSearch = Entry(window, width=30)
 txtReplace = Entry(window, width=30)
-
+txtLog = Text(window, width=35, height=6)
+btnReplace = Button(window, text="Replace", command=btnReplace_Click)
+# txtLog.insert(END,"Just a text Widget\nin two lines\nfjhsdjhfasdf\ndjfhksjhfksd\n")
 # layout form
-lblSearch.grid(row=0, column=0, sticky='w')
-lblReplace.grid(row=1, column=0, sticky='w')
-txtSearch.grid(row=0, column=1)
-txtReplace.grid(row=1, column=1)
-
-btnReplace = Button(window, text="Replace", command=btnReplace_Click).grid(row=2, column=1)
+lblPath.grid(row=0, column=0, sticky='w')
+lblSearch.grid(row=1, column=0, sticky='w')
+lblReplace.grid(row=2, column=0, sticky='w')
+lblLog.grid(row=4,column=0, sticky='w')
+txtPath.grid(row=0, column=1, sticky='w')
+txtSearch.grid(row=1, column=1, sticky='w')
+txtReplace.grid(row=2, column=1, sticky='w')
+txtLog.grid(row=5, column=1, columnspan=2, sticky='w')
+btnReplace.grid(row=3, column=1, sticky='w')
 
 window.mainloop()
 
